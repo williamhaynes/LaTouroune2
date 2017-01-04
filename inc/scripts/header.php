b3037f39d9a22fea381f8d06fdfc9acc98c305cb
williamhaynes <w.haynes@rgu.ac.uk>
2017-01-04 16:47:31 +0000

    modify all header texts

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -9,7 +9,7 @@ session_start(); //start the session
     <title>La Touroune</title>
     <link rel="stylesheet" type="text/css" href="../../style/style.css">
     <!-- Font link -->
-    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai|Tangerine" rel="stylesheet">
 </head>
 <header>
     <nav id="topNavBar">
