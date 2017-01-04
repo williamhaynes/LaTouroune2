7ce3c7c15bbe2b8e25d1a99c8e4bc705e6d9ff63
williamhaynes <w.haynes@rgu.ac.uk>
2017-01-04 16:25:50 +0000

    adding descriptor

--- inc/scripts/header.php
+++ inc/scripts/header.php
@@ -14,7 +14,7 @@ session_start(); //start the session
 <header>
     <nav id="topNavBar">
         <div id="leftDiv">
-
+            <h1>leftDiv</h1>
         </div>
         <div id="centreDiv">
             <h1>La Touroune</h1>
